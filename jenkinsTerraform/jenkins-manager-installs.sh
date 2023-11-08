4f5a2996c2f0b7cc35c7b6825a537653d87687fb
belindadunu <belindadunu@gmail.com>
2023-11-08 04:33:38 +0000

    add jenkinsTerraform

---
+++ jenkinsTerraform/jenkins-manager-installs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#################### Install Java ####################
+sudo apt-get update -y
+apt-get install -y openjdk-8-jre -y
+sudo apt-get update -y
+
+#################### Install Jenkins ####################
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update -y
+
+#################### Install and start Jenkins ####################
+sudo apt-get install -y fontconfig openjdk-17-jre jenkins
+sudo systemctl start jenkins
+
+#################### Print initial admin password ####################
+echo "Jenkins initial admin password: " 
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#################### Done! ####################
+echo "Jenkins successfully insalled!"
+echo "########################################"
+echo "Install the Docker pipeline plugin using the web interface"
+
+exit 0
