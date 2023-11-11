d52a51676fc627037d909f1913ecd787e74fd964
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2023-11-10 21:52:32 -0500

    Adding vpc and variables to tf frontend

--- frontend/initTerraformFrontend/variables.tf
+++ frontend/initTerraformFrontend/variables.tf
@@ -4,3 +4,23 @@ variable aws_secret_key {}
 variable "deployment8_region" {
   default = "us-east-1" 
 }
+
+variable "deployment8_vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "deployment8_pubSubnet01_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "deployment8_pubSubnet02_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "deployment8_az1" {
+  default = "us-east-1a"
+}
+
+variable "deployment8_az2" {
+  default = "us-east-1b"  
+}
---
+++ frontend/initTerraformFrontend/vpc.tf
@@ -0,0 +1,128 @@
+#################### VPC ####################
+resource "aws_vpc" "deployment8_vpc" {
+  cidr_block = var.deployment8_vpc_cidr
+  tags = {
+    "Name" = "deployment8-vpc"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.deployment8_vpc.id
+}
+
+#################### Subnets ####################
+resource "aws_subnet" "deployment8_pubsub_a" {
+  vpc_id            = aws_vpc.deployment8_vpc.id
+  cidr_block        = var.deployment8_pubSubnet01_cidr
+  availability_zone = var.deployment8_az1
+
+  tags = {
+    "Name" = "pubsubA"
+  }
+}
+
+resource "aws_subnet" "deployment8_pubsub_b" {
+  vpc_id            = aws_vpc.deployment8_vpc.id
+  cidr_block        = var.deployment8_pubSubnet02_cidr
+  availability_zone = var.deployment8_az2
+
+  tags = {
+    "Name" = "pubsubB"
+  }
+}
+
+#################### Rte Tble Associations ####################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.deployment8_vpc.id
+}
+
+resource "aws_route_table_association" "public_subnet_a" {
+  subnet_id      = aws_subnet.deployment8_pubsub_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_subnet_b" {
+  subnet_id      = aws_subnet.deployment8_pubsub_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "elastic-ip" {
+  domain = "vpc"
+}
+
+resource "aws_internet_gateway" "deployment8_igw" {
+  vpc_id = aws_vpc.deployment8_vpc.id
+}
+
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.deployment8_igw.id
+}
+
+resource "aws_security_group" "http_alb" {
+  name        = "httpalb"
+  description = "HTTP ALB traffic"
+  vpc_id      = aws_vpc.deployment8_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "HTTP ALB ingresstraffic"
+  }
+}
+
+resource "aws_security_group" "ingress_traffic_frontend" {
+  name        = "ingress-app-frontend"
+  description = "Allow ingress to frontend of APP"
+  vpc_id      = aws_vpc.deployment8_vpc.id
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "ingress-app-frontend-security-group"
+  }
+}
+
+resource "aws_security_group" "ingress_traffic_backend" {
+  name        = "ingress-app-backend"
+  description = "Allow ingress to backend of APP"
+  vpc_id      = aws_vpc.deployment8_vpc.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Name" = "ingress-app-backend-security-group"
+  }
+}
